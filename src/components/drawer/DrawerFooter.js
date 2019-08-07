7f5143d67f3a29bda86549147b1bf60971ac5c21
Hai <wordpree.dev@gmail.com>
2019-08-07 18:21:44 +1000

    added social media share link

--- src/components/drawer/DrawerFooter.js
+++ src/components/drawer/DrawerFooter.js
@@ -33,17 +33,38 @@ const useStyle = makeStyles({
     width: "60%",
     margin: "0 auto",
     backgroundColor: "#2F6F80"
+  },
+  socialIcon: {
+    display: "flex",
+    textDecoration: "none",
+    color: "inherit"
   }
 });
 export default function DrawerFooter() {
   const classes = useStyle();
-  const icons = [Facebook, Linkedin, Twitter, Pinterest];
+  const fbHref =
+    "https://www.facebook.com/dialog/share?app_id=336368817292380&display=popup&href=https://spa-demo-e4d44.firebaseapp.com/&redirect_uri=https://qld-natureside.netlify.com/";
+  const twHref =
+    "https://twitter.com/intent/tweet?text=Here we go&url=https://qld-natureside.netlify.com/&hashtags=reactjs,spa";
+  const lkiHref =
+    "https://www.linkedin.com/shareArticle?url=https://qld-natureside.netlify.com/&mini=true";
+  const pinHref =
+    "//pinterest.com/pin/create/link?url=https://qld-natureside.netlify.com/&media=https://spa-demo-e4d44.firebaseapp.com/static/media/snapshot.60fdb371.jpg&description=SAP with reactjs";
 
+  const icons = [Facebook, Linkedin, Twitter, Pinterest];
+  const href = [fbHref, lkiHref, twHref, pinHref];
   const listItems = icons.map((Icon, index) => (
     <ListItem key={index}>
-      <IconButton className={classes.iconBtn}>
-        <Icon />
-      </IconButton>
+      <a
+        className={classes.socialIcon}
+        target="_blank"
+        rel="noopener noreferrer"
+        href={href[index]}
+      >
+        <IconButton className={classes.iconBtn}>
+          <Icon />
+        </IconButton>
+      </a>
     </ListItem>
   ));
 
